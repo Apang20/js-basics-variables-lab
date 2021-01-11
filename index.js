5837b753b65daea119728098d9f2274645b6a78a
github username <github email address>
2021-01-11 15:43:26 -0600

    commit tree 3d4600961295b8fe739f3da452b18cfcf04b0589

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith';// Code your solution in this file!
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
